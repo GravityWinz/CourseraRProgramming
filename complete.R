95e3caf2391f54a7803b36316a275a4602355b5e
GravityWinz <mlutze@gmail.com>
2016-07-17 14:13:10 -0600

    Commit

--- complete.R
+++ complete.R
@@ -1,1 +1,27 @@
-complete <- function (directory, pollutent, id = 1:332) {
+complete <- function (directory, id = 1:332) {
+
+	
+	df <- data.frame(
+		id=integer(),
+		nobs=integer(),
+		stringsAsFactors=FALSE) 
+
+	
+	for (x in id)	{
+	
+		## thiink that there is a better way to do this
+		file <- paste(directory,"/", sprintf("%03d.csv",  x) , sep="")
+		d <- read.csv(file)
+		
+		dc <- d[complete.cases(d), ]
+		
+		l <- nrow(dc)
+		
+		df <- rbind(df, c(x,l) )
+		
+	}
+
+	colnames(df) <- c("id","nobs")
+	
+	df
+}
